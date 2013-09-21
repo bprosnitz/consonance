bdfc283b7660a455126e3ff417b9e49e8fdbe988
Benjamin Prosnitz <bprosnitz@gmail.com>
2013-09-21 16:00:16 -0700

    Use chai.js

--- test/note.js
+++ test/note.js
@@ -1,11 +1,16 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var consonance = require('../src/consonance').consonance;
 
 describe('Note', function() {
     describe('Constructors', function() {
+        var testNote = function(name, noteName, octave) {
+            note = consonance.Note(name);
+            expect(note.noteName()).to.equal(noteName);
+            expect(note.octave()).to.equal(octave);
+        };
         describe('without octave set', function() {
             it('A', function() {
-                var x = consonance.Note('A');
+                testNote('A', 'A', null);
             });
         });
     });
