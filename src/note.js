619c1357392edd270c589a44d030323b54bf00c4
Benjamin Prosnitz <bprosnitz@gmail.com>
2013-09-21 17:03:31 -0700

    Added tests of initial note functionality

--- src/note.js
+++ src/note.js
@@ -23,19 +23,23 @@ exports.Note = function(param) {
             var reResult = re.exec(name);
             private.noteName = reResult[1];
             if (reResult[2].length > 0) {
-                private.octave = int(reResult[2]);
+                private.octave = parseInt(reResult[2]);
             }
 
-            var index = constants.noteIndicies[private.noteName[0]];
+            var index = constants.noteIndicies[private.noteName[0].toLowerCase()];
             for (var i = 1; i < private.noteName.length; ++i) {
                 var char = private.noteName[i];
                 if (char == '#') {
                     ++index
                 } else if (char == 'b' || char == 'B') {
-                    ++index;
+                    --index;
                 } else {
                     throw new Error('Invalid character: ' + char);
                 }
+            }
+            index = index % 12;
+            if (index < 0) {
+                index += 12;
             }
             private.index = index;
         }
@@ -57,8 +61,8 @@ exports.Note = function(param) {
         index: function() {
             return private.index;
         },
-        equal: function() {
-            return index == index && octave == octave;
+        equals: function(other) {
+            return this.index() == other.index() && this.octave() == other.octave();
         },
         from: from
     };
--- test/note.js
+++ test/note.js
@@ -3,14 +3,76 @@ var consonance = require('../src/consonance').consonance;
 
 describe('Note', function() {
     describe('Constructors', function() {
-        var testNote = function(name, noteName, octave) {
+        var testConstructorSetsCorrectValues = function(name, noteName, octave) {
             note = consonance.Note(name);
             expect(note.noteName()).to.equal(noteName);
             expect(note.octave()).to.equal(octave);
+            expect(note.index()).to.be.a('number');
         };
         describe('without octave set', function() {
             it('A', function() {
-                testNote('A', 'A', null);
+                testConstructorSetsCorrectValues('A', 'A', null);
+            });
+            it('A#', function() {
+                testConstructorSetsCorrectValues('A#', 'A#', null);
+            });
+            it('Bb', function() {
+                testConstructorSetsCorrectValues('Bb', 'Bb', null);
+            });
+            it('C', function() {
+                testConstructorSetsCorrectValues('C', 'C', null);
+            });
+            it('Db', function() {
+                testConstructorSetsCorrectValues('Db', 'Db', null);
+            });
+            it('E', function() {
+                testConstructorSetsCorrectValues('E', 'E', null);
+            });
+            it('E#', function() {
+                testConstructorSetsCorrectValues('E#', 'E#', null);
+            });
+            it('F', function() {
+                testConstructorSetsCorrectValues('F', 'F', null);
+            });
+            it('G##', function() {
+                testConstructorSetsCorrectValues('G##', 'G##', null);
+            });
+            it('G#b', function() {
+                testConstructorSetsCorrectValues('G#b', 'G#b', null);
+            });
+        });
+        describe('Constructor with octave', function() {
+            it('A0', function() {
+                testConstructorSetsCorrectValues('A0', 'A', 0);
+            });
+            it('C#9', function() {
+                testConstructorSetsCorrectValues('C#9', 'C#', 9);
+            });
+            it('Gb3', function() {
+                testConstructorSetsCorrectValues('Gb3', 'Gb', 3);
+            });
+        });
+        describe('equal', function() {
+            it('A equals A', function() {
+                expect(consonance.Note('A').equals(consonance.Note('A'))).to.be.true;
+            });
+            it('A not equals A#', function() {
+                expect(consonance.Note('A').equals(consonance.Note('A#'))).to.be.false;
+            });
+            it('Bb1 equals Cbb1', function() {
+                expect(consonance.Note('Bb1').equals(consonance.Note('Cbb1'))).to.be.true;
+            });
+            it('B##3 equals Db3', function() {
+                expect(consonance.Note('B##1').equals(consonance.Note('Db1'))).to.be.true;
+            });
+            it('E##1 not equals F1', function() {
+                expect(consonance.Note('E##1').equals(consonance.Note('F1'))).to.be.false;
+            });
+            it('A1 not equals A2', function() {
+                expect(consonance.Note('A1').equals(consonance.Note('A2'))).to.be.false;
+            });
+            it('A1 not equals A', function() {
+                expect(consonance.Note('A1').equals(consonance.Note('A'))).to.be.false;
             });
         });
     });
