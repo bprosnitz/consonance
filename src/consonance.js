4c7dc5a3dfb488ad18ff0891803877832466f5e7
Benjamin Prosnitz <bprosnitz@gmail.com>
2013-09-28 12:27:12 -0700

    Missing list field

--- src/consonance.js
+++ src/consonance.js
@@ -2,10 +2,12 @@ var note = require('./note');
 var interval = require('./interval');
 var tuning = require('./tuning');
 var scale = require('./scale');
+var list = require('./list');
 
 exports.consonance = {
     Note: note.Note,
     Interval: interval.Interval,
     Tuning: tuning.Tuning,
-    Scale: scale.Scale
+    Scale: scale.Scale,
+    List: list.List
 };
